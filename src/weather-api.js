24ce4c8382205d3f71538bf335bcb49df80a1710
Bluesman98 <papaderos1998@gmail.com>
2022-08-20 12:27:26 +0300

    fetch url edit

--- src/weather-api.js
+++ src/weather-api.js
@@ -4,8 +4,8 @@ const key = "82d187774de3e295c9a75f3749708fe9";
 async function findCity(city) {
   try {
     let response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`,
-      { method: "GET", headers: { accept: "aplication/json" } }
+      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`,
+      {mode: 'cors'}
     );
     let data = await response.json();
     return data[0];
@@ -17,7 +17,7 @@ async function findCity(city) {
 async function getWeather(lat, lon, units = "metric") {
   let response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${key}`,
-    { method: "GET", headers: { accept: "aplication/json" } }
+    {mode: 'cors'}
   );
   let data = await response.json();
   return data;
